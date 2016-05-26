5515a51d9d8508b193326a7ee53ff40746dd7033
fttz8706 <yjm5153@126.com>
2016-05-26 18:13:21 +0800

    extends

---
+++ src/main/java/com/yjm/study/extend/Father.java
@@ -0,0 +1,19 @@
+package com.yjm.study.extend;
+
+/**
+ * Created by yangjinming on 16/5/26.
+ */
+public class Father {
+
+    private int age;
+    private String name;
+
+    public Father(){
+
+    }
+
+    public Father(int age, String name){
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ src/main/java/com/yjm/study/extend/Son.java
@@ -0,0 +1,19 @@
+package com.yjm.study.extend;
+
+/**
+ * Created by yangjinming on 16/5/26.
+ */
+public class Son extends Father {
+
+    private String school;
+
+    // 如果父类重载了构造函数,则它必须提供一个无参构造函数,否则不能通过编译
+    public Son(){
+
+    }
+
+    public Son(int age, String name, String school){
+        super(age, name);
+        this.school = school;
+    }
+}
